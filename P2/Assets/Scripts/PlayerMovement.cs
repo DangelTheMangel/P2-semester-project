f6113c98b10ebd51198905407c0ebf284888206f
Lucas <lfmdesigner@gmail.com>
2022-03-31 13:40:19 +0200

    CASPER

--- P2/Assets/Scripts/PlayerMovement.cs
+++ P2/Assets/Scripts/PlayerMovement.cs
@@ -1,13 +1,16 @@
+//based on https://youtu.be/mbzXIOKZurA
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public Transform movePoint;
     // Start is called before the first frame update
     void Start()
     {
-        
+        movePoint.parent = null;
     }
 
     // Update is called once per frame
