d81b6494216f477356bc45e5f05c0b8dcd958ed4
Lucas <lfmdesigner@gmail.com>
2022-04-06 15:05:47 +0200

    Player can rotate*
    however, the player will continue to rotate while the key is pressed. and forward movement is still not relative to the player direction

--- P2/Assets/Scripts/PlayerMovement.cs
+++ P2/Assets/Scripts/PlayerMovement.cs
@@ -9,6 +9,7 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 100f;
     public float collisionCheckerSize = .2f;
     public Transform movePoint;
+    public Transform playerSprite;
 
     private PlayerControls playerControls;
 
@@ -41,16 +42,20 @@ public class PlayerMovement : MonoBehaviour
 
         if (Vector3.Distance(transform.position, movePoint.position) <= .05f)
         {
-
-            if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
+            if (Mathf.Abs(playerControls.Freemovement.Rotate.ReadValue<float>()) == 1f)
             {
-                if(!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), collisionCheckerSize, whatStopsMovement))
-                {
-                    movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
-                }
+                Debug.Log(playerControls.Freemovement.Rotate.ReadValue<float>());
+                transform.eulerAngles += new Vector3(0, 0, (playerControls.Freemovement.Rotate.ReadValue<float>() * 90));
             }
+                /*if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
+                {
+                    if(!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), collisionCheckerSize, whatStopsMovement))
+                    {
+                        movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+                    }
+                }*/
 
-            if (playerControls.Freemovement.Move.triggered)
+                if (playerControls.Freemovement.Move.triggered)
             {
                 if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, 1f, 0f), collisionCheckerSize, whatStopsMovement))
                 {
