05fd66089cf9e09eee587e6a6ed9ba89de51287d
Kiliopo <94614380+Kiliopo@users.noreply.github.com>
2022-04-06 11:03:46 +0200

    Update to SOundManager

--- P2/Assets/Scripts/SoundManager.cs
+++ P2/Assets/Scripts/SoundManager.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static SoundManager Instance;
+
+    [SerializeField] private AudioSource _musicSource, _effectSource;
+
+    private void Awake()
     {
-        
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlaySound(AudioClip clip)
     {
-        
+        _effectSource.PlayOneShot(clip);
     }
+
 }
