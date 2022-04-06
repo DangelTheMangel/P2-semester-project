f6d54582d5648732387a2ab562d39775bab6348e
Kiliopo <94614380+Kiliopo@users.noreply.github.com>
2022-04-06 11:08:07 +0200

    Få endringer inde på SoundManager

--- P2/Assets/Scripts/SoundManager.cs
+++ P2/Assets/Scripts/SoundManager.cs
@@ -12,8 +12,7 @@ using UnityEngine;
 public class SoundManager : MonoBehaviour
 {
     public static SoundManager instance;
-    [SerializeField]
-    private AudioSource musicSource, sfxSource;
+    [SerializeField] private AudioSource musicSource, effectSource;
     public void Awake()
     {
         //tjekker om der er en instance og hvis der ikke er
@@ -35,7 +34,7 @@ public class SoundManager : MonoBehaviour
     /// <param name="audioClip"></param>
     public void playSFX(AudioClip audioClip)
     {
-        sfxSource.PlayOneShot(audioClip);
+        effectSource.PlayOneShot(audioClip);
     }
 
     public void changeMasterVolume(float value)
@@ -48,14 +47,14 @@ public class SoundManager : MonoBehaviour
         musicSource.volume = value;
     }
 
-    public void changeSFXVolume(float value)
+    public void changeEffectVolume(float value)
     {
-        sfxSource.volume = value;
+        effectSource.volume = value;
     }
 
-    public void toggleSFX()
+    public void toggleEffect()
     {
-        sfxSource.mute = !sfxSource.mute;
+        effectSource.mute = !effectSource.mute;
     }
 
     public void toggleMusic()
