eb6e06b144d4b780e3867539d1e9cf985a8238aa
Lykke-Gaming <stellanemil@gmail.com>
2022-04-06 11:35:07 +0200

    TrapActivator
    Trap Activator Script

---
+++ P2/Assets/Scripts/TrapActivator.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapActivator : MonoBehaviour
+{
+    public bool activateTrap = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            activateTrap = true;
+        }
+
+}
