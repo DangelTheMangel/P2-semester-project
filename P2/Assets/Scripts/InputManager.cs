91639728c97340319550c46573a56895652eea2d
Lucas <lfmdesigner@gmail.com>
2022-04-07 11:31:11 +0200

    Completed input manager(?)

--- P2/Assets/Scripts/InputManager.cs
+++ P2/Assets/Scripts/InputManager.cs
@@ -1,18 +1,53 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+//based on https://youtu.be/XUx_QlJpd0M
 public class InputManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    #region Events
+    public delegate void StartTouch(Vector2 position, float time);
+    public event StartTouch OnStartTouch;
+    public delegate void EndTouch(Vector2 postion, float time);
+    public event EndTouch OnEndTouch;
+    #endregion
+
+    private PlayerControls playerControls;
+    private Camera mainCamera;
+
+
+    private void Awake()
+    {
+        playerControls = new PlayerControls();
+        mainCamera = Camera.main;
+    }
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+    private void OnDisable()
+    {
+        playerControls.Disable();
+    }
     void Start()
     {
-        
+        playerControls.Touch.PrimaryContact.started += ctx => StartTouchPrimary(ctx);
+        playerControls.Touch.PrimaryContact.canceled += ctx => EndTouchPrimary(ctx);
     }
-
-    // Update is called once per frame
-    void Update()
+    private void StartTouchPrimary(InputAction.CallbackContext context)
     {
-        
+        if (OnStartTouch != null) OnStartTouch(Utils.ScreenToWorld(mainCamera, playerControls.Touch.PrimaryPosition.ReadValue<Vector2>()), (float)context.startTime);
+    }
+    private void EndTouchPrimary(InputAction.CallbackContext context)
+    {
+        if (OnEndTouch != null) OnEndTouch(Utils.ScreenToWorld(mainCamera, playerControls.Touch.PrimaryPosition.ReadValue<Vector2>()), (float)context.time);
+    }
+    /// <summary>
+    /// Returns position of the finger, for the trail renderer.
+    /// </summary>
+    /// <returns></returns>
+    public Vector2 PrimaryPosition()
+    {
+        return Utils.ScreenToWorld(mainCamera, playerControls.Touch.PrimaryPosition.ReadValue<Vector2>());
     }
 }
