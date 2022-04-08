719421eaeb22f13bad5b4daefeed2f0b5c4a42f4
Lykke-Gaming <stellanemil@gmail.com>
2022-04-08 10:38:16 +0200

    MainMenu
    Main freaking menu

--- P2/Assets/Scripts/StartMenu.cs
+++ P2/Assets/Scripts/StartMenu.cs
@@ -5,24 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class StartMenu : MonoBehaviour
 {
-     public void Playgame ()
-   {
+    public void Playgame()
+    {
+        //Change Scene when pressing start
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        //Or one where the name of the level is used to load (just remove //)
+        //SceneManager.LoadScene("Test Level") ; 
+    }
+    public void QuitGame ()
+    {
 
-   //Change Scene when pressing start
-   SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); 
-   //Or one where the name of the level is used to load (just remove //)
-   //SceneManager.LoadScene("Test Level") ; 
+        // Close the game when pressing exit
 
+        //Display in log that the game is quitting
 
-}
-public void QuitGame ()
-{
-
-// Close the game when pressing exit
-
-//Display in log that the game is quitting
-
-Debug.Log ("Quit");
-Application.Quit();
-}
+        Debug.Log("Quit");
+        Application.Quit();
+    }
 }
