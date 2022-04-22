655f96986a76d52ab6dca0d44bf294125adf936b
Christian Randleff <54103424+DangelTheMangel@users.noreply.github.com>
2022-04-22 14:19:38 +0200

    Can change the trap control type

--- P2/Assets/ControlsEnabler.cs
+++ P2/Assets/ControlsEnabler.cs
@@ -1,18 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ControlsEnabler : MonoBehaviour
 {
+    public bool shakeOn = true;
+    [SerializeField]
+    string[] buttonText = { "Shake On", "Tilted On" };
+    [SerializeField]
+    Text buttonTextObj;
+    [SerializeField]
+    GameObject GameManagerPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        turnOn();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void tooglecontrols()
     {
-        
+        if (shakeOn)
+        {
+            shakeOn = false;
+            turnOff();
+            buttonTextObj.text = buttonText[1];
+
+        }
+        else
+        {
+            shakeOn = true;
+            turnOn();
+            buttonTextObj.text = buttonText[0];
+        }
+    }
+    public void turnOff()
+    {
+        GameManagerPrefab.GetComponent<GameManganer>().shake = false;
+    }
+    public void turnOn()
+    {
+        GameManagerPrefab.GetComponent<GameManganer>().shake = true;
     }
 }
