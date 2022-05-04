05fc04311d62d5eb75a8c7dca2bf11c3cd871200
Christian Randleff <54103424+DangelTheMangel@users.noreply.github.com>
2022-05-04 14:54:40 +0200

    fixed up debug menu

--- P2/Assets/Scripts/Player/PlayerMovement.cs
+++ P2/Assets/Scripts/Player/PlayerMovement.cs
@@ -43,7 +43,7 @@ public class PlayerMovement : MonoBehaviour
     Vector3 bias;
 
     [Header("Debug stuff")]
-    Text debugDisplay;
+    public Text debugDisplay;
 
     private void Awake()
     {
