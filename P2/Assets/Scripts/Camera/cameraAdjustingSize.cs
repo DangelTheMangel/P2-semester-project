51acbd5113e3b8d980350f4fac81d918c98d8af4
Christian Randleff <54103424+DangelTheMangel@users.noreply.github.com>
2022-05-27 12:24:10 +0200

    change Camera bullshit

--- P2/Assets/Scripts/Camera/cameraAdjustingSize.cs
+++ P2/Assets/Scripts/Camera/cameraAdjustingSize.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraBullshit : MonoBehaviour
+public class cameraAdjustingSize : MonoBehaviour
 {
     //script credit: gamedesigntheory blogspot
     void Start()
