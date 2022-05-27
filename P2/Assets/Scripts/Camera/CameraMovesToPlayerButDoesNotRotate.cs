0ccb11252a779bc76f6d81f61024ac2cb25628ff
Christian Randleff <54103424+DangelTheMangel@users.noreply.github.com>
2022-05-27 13:38:06 +0200

    Made a comment and removed magic number

--- P2/Assets/Scripts/Camera/CameraMovesToPlayerButDoesNotRotate.cs
+++ P2/Assets/Scripts/Camera/CameraMovesToPlayerButDoesNotRotate.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 public class CameraMovesToPlayerButDoesNotRotate : MonoBehaviour
 {
     GameObject Gamer;
+    Vector3 offset = new Vector3(0, 0, -10);;
 
     // Start is called before the first frame update
     void Start()
     {
+        //finds the player
         Gamer = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Gamer.transform.position + new Vector3(0, 0, -10);
+        //move the Cameras transform to the players transform and add offset
+        transform.position = Gamer.transform.position + offset;
     }
 }
