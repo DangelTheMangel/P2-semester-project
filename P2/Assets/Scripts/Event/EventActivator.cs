70a9a668ab1b4504ccf0a217bb840ba7d706250f
Christian Randleff <54103424+DangelTheMangel@users.noreply.github.com>
2022-05-28 12:59:00 +0200

    added coment and cleaned op

--- P2/Assets/Scripts/Event/EventActivator.cs
+++ P2/Assets/Scripts/Event/EventActivator.cs
@@ -12,12 +12,15 @@ public class EventActivator : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D collision)
     {
+        //checks if the collide object are the player
         if (collision.tag == "Player") //her er de seje ting der sker når den aktiveres
         {
+            //create the prefab
             GameObject obj =
             Instantiate(Prefab, this.transform.position, this.transform.rotation);
-
+            //set the prefab parrent to be this gameobjct
             obj.transform.parent = this.gameObject.transform;
+            //if there are a sound effect it play it
             if (soundEffectName != "") {
                 SoundManager.instance.playEffect(gameObject, soundEffectName);
             }
